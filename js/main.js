331b9edc951f9360afe8a0cb265a9d196452682f
alejandroBarua <2baruaalejandro95@gmail.com>
2021-02-15 16:53:14 -0300

    puedo agregar tareas y seleccionarlas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import pointTime from "./point.js";
 import  newTask from "./newTask.js";
+import  {colorSelect} from "./newTask.js";
 
 
 document.addEventListener("DOMContentLoaded", e => {
-    newTask(".add-btn")
+    newTask(".add-btn", ".delete-btn")
 });
 
 
--- js/newTask.js
+++ js/newTask.js
@@ -1,7 +1,8 @@
 
+export let colorSelect;
 
 
-export default function newTask(addBtn) {
+export default function newTask(addBtn, deleteBtn) {
     
     document.addEventListener("click", e => {
 
@@ -10,7 +11,45 @@ export default function newTask(addBtn) {
             let color = document.querySelector(".color-input").value;
             let text = document.querySelector(".name-input").value;
 
+            let $list = document.querySelector(".list-task"),
+                $div = document.createElement("div"),
+                $button = document.createElement("button"),
+                $p = document.createElement("p");
+
+            $div.style.backgroundColor = color;
+            colorSelector($div);
+            $div.addEventListener("click", () => {
+                console.log('select');
+                console.log(colorSelect);
+                colorSelector($div)});
+
+            $button.textContent = "x";
+            $button.classList.add("delete-btn");
+            $p.textContent = text;
+
+            $div.appendChild($button);
+            $div.appendChild($p);
+
+            $list.prepend($div);
+
+        }
+
+         if(e.target.matches(deleteBtn)){
+         
+            console.log(deleteBtn);
+        
+        
         }
     
     });
+}
+
+function colorSelector($div) {
+    
+    let $divPrev = document.querySelector(".select-task");
+    if($divPrev != null) $divPrev.classList.remove("select-task");
+                
+    $div.classList.add("select-task");
+    colorSelect = $div.style.backgroundColor;
+
 }
