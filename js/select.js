ef0bb70a0f1c41fe225e921853a12cff49d7fc89
alejandroBarua <2baruaalejandro95@gmail.com>
2021-02-19 19:40:09 -0300

    se agrego trick de pintado automatico

--- js/select.js
+++ js/select.js
@@ -23,13 +23,15 @@ export default function selectBlock() {
 
         el.addEventListener("click", e => {
 
+            
             let $task = document.querySelector(".select-task");
             let $block = e.target;
             
             if($task != null){
+                    
                 let color = $task.style.backgroundColor;
                 
-                 if($block.classList.length == 3){
+                if($block.classList.length == 3){
                     if($block.classList[1] != "point"){
                 
                         $block.classList.remove(`${$block.classList[1]}`);
@@ -47,8 +49,123 @@ export default function selectBlock() {
                     el.style.backgroundColor = color;
                     $block.classList.add(`${$task.children[1].textContent.replace(/ /g, "")}`);
                 }
+
+                
+                let $trick = document.querySelector(".trick");
+
+                if($trick.classList.contains("select-trick")){
+
+                    let $first = document.querySelector(".first");
+
+                    if($first == null){
+                    
+                        $block.classList.add("first");
+                    }
+                    else if($first != $block && $first.style.backgroundColor == $block.style.backgroundColor){
+
+                        console.log($first.classList[0], $block.classList[0]);
+
+                        let dayFirst = dayToNumber($first.classList[0].slice(0,3));
+                        let numberFirst = parseInt($first.classList[0].slice(3,5));
+
+                        let daySecond = dayToNumber($block.classList[0].slice(0,3));
+                        let numberSecond = parseInt($block.classList[0].slice(3,5));
+
+                        if(dayFirst > daySecond){
+                            let a = daySecond;
+                            daySecond = dayFirst;
+                            dayFirst = a;
+                        }
+                        if(numberFirst > numberSecond){
+                            let a = numberSecond;
+                            numberSecond = numberFirst;
+                            numberFirst = a;
+                        }
+
+                        for(let i= dayFirst; i <= daySecond; i++){
+
+                            for(let j = numberFirst; j <= numberSecond; j++){
+
+                                let $blockTrick = document.querySelector(`.${numbeToDay(i)}${j}`);
+                                $blockTrick.style.backgroundColor= color;
+
+                            }
+                        }
+                        
+                        
+                        $first.classList.remove("first");
+                    }
+                    else{
+                        $first.classList.remove("first");
+                    }
+
+                }
             }
+                
         });
     });
     
+}
+
+
+function numbeToDay(number) {
+
+    let day;
+     
+    switch (number) {
+        case 0:
+            day = "sun";
+            break;
+        case 1:
+            day = "mon";
+            break;
+        case 2:
+            day = "tue";
+            break;
+        case 3:
+            day = "wed";
+            break;
+        case 4:
+            day = "thu"
+            break;
+        case 5:
+            day = "fri";
+            break;
+        case 6:
+            day = "sat";
+            break;
+    }  
+
+    return day;
+}
+
+function dayToNumber(day) {
+
+    let number;
+     
+    switch (day) {
+        case "sun":
+            number = 0;
+            break;
+        case "mon":
+            number = 1;
+            break;
+        case "tue":
+            number = 2;
+            break;
+        case "wed":
+            number = 3;
+            break;
+        case "thu":
+            number = 4;
+            break;
+        case "fri":
+            number = 5;
+            break;
+        case "sat":
+            number = 6;
+            break;
+    }  
+
+    return number;
 }
